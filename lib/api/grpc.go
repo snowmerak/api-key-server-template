b0ef586c58753d6c2d2eb5a8397665fffbaf17ad
snowmerak <snowmerak@outlook.com>
2025-03-24 21:07:31 +0900

    Remove files

--- lib/api/grpc.go
+++
@@ -1,5 +0,0 @@
-package api
-
-import v1 "github.com/snowmerak/api-key-server-template/gen/api/authorizer/v1"
-
-type GRPC v1.AuthorizerServer
--- lib/api/http.go
+++
@@ -1,7 +0,0 @@
-package api
-
-import "net/http"
-
-type HTTP interface {
-	Authorize(w http.ResponseWriter, r *http.Request) error
-}
