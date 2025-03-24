945cb2cba026ab09ff1b645bbf98173f4c94b447
snowmerak <snowmerak@outlook.com>
2025-03-24 22:55:51 +0900

    Add asynchronous and synchronous server interfaces for improved request handling

--- lib/api/api.go
+++ lib/api/api.go
@@ -6,7 +6,11 @@ import (
 	v1 "github.com/snowmerak/api-key-server-template/gen/api/authorizer/v1"
 )
 
-type Server interface {
+type AsyncServer interface {
+	Subscribe(ctx context.Context, handler func(request *v1.AuthorizerRequest) *v1.AuthorizerResponse) error
+}
+
+type SyncServer interface {
 	Reply(ctx context.Context, request *v1.AuthorizerRequest) *v1.AuthorizerResponse
 }
 
